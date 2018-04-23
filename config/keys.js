389b556bc4a5c18b2e1c342dfb37bcdb0377c9c6
Egor <egor.piskunou@gmail.com>
2018-04-23 20:24:55 +0300

    Create jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://admin:908090@ds157538.mlab.com:57538/devconnector'
+    mongoURI: 'mongodb://admin:908090@ds157538.mlab.com:57538/devconnector',
+    secretOrKey: 'o2i5uo4i3hkjfdlkvc'
 }

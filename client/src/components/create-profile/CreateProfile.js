ed4b3e93563c818eab228d9b44ea319905fd0b7c
Egor <egor.piskunou@gmail.com>
2018-05-05 19:19:53 +0300

    Create Profile Functionality

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
 import SelectListGroup from '../common/SelectListGroup';
+import { createProfile } from '../../actions/profileActions';
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -31,9 +33,32 @@ class CreateProfile extends Component {
     this.onChange = this.onChange.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({ errors: nextProps.errors })
+    } 
+  }
+
   onSubmit(e) {
     e.preventDefault();
 
+    const profileDate = {
+      handle: this.state.handle,
+      company: this.state.company,
+      website: this.state.website,
+      location: this.state.location,
+      status: this.state.status,
+      skills: this.state.skills,
+      githubusername: this.state.githubusername,
+      bio: this.state.bio,
+      twitter: this.state.twitter,
+      facebook: this.state.facebook,
+      linkedin: this.state.linkedin,
+      youtube: this.state.youtube,
+      instagram: this.state.instagram
+    }
+
+    this.props.createProfile(profileDate, this.props.history);
   }
 
   onChange(e) {
@@ -187,8 +212,10 @@ class CreateProfile extends Component {
                 />
 
                 <div className="mb-3">
-                  <button onClick={() => {
-                    this.setState(prevStat => ({
+                  <button
+                    type="button"
+                    onClick={() => {
+                    this.setState(prevState => ({
                       displaySocialInputs: !prevState.displaySocialInputs
                     }))
                   }} className="btn btn-light">
@@ -217,4 +244,4 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile)
+export default connect(mapStateToProps, { createProfile })(withRouter(CreateProfile));

43f9e38df8ae6e31f10ed5f2605abfff66a1677c
Egor <egor.piskunou@gmail.com>
2018-05-09 09:55:36 +0300

    Comment Display & Delete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
+import CommentFeed from './CommentFeed';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
@@ -10,18 +12,19 @@ class Post extends Component {
   componentDidMount() {
     this.props.getPost(this.props.match.params.id);
   }
-  
 
   render() {
     const { post, loading } = this.props.post;
     let postContent;
 
-    if(post === null || loading || Object.keys(post).length === 0) {
-      postContent = <Spinner />
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />;
     } else {
       postContent = (
         <div>
-          <PostItem post={post} showAcions={false}/>
+          <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       );
     }
@@ -39,14 +42,14 @@ class Post extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 Post.propTypes = {
   getPost: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   post: state.post

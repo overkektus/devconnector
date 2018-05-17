cee9ba78b69a3e68d7833bce45f0b84beb04effb
Egor <egor.piskunou@gmail.com>
2018-05-17 20:53:33 +0300

    fix reducer Profile

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -10,28 +10,28 @@ import {
 const initialState = Store;
 
 export default function(state = initialState, action) {
-  switch(action.type) {
+  switch (action.type) {
     case PROFILE_LOADING:
       return {
         ...state,
         loading: true
-      }
+      };
     case GET_PROFILE:
       return {
         ...state,
         profile: action.payload,
         loading: false
       };
-    case CLEAR_CURRENT_PROFILE:
-      return {
-        ...state,
-        profile: null
-      };
     case GET_PROFILES:
       return {
         ...state,
         profiles: action.payload,
         loading: false
+      };
+    case CLEAR_CURRENT_PROFILE:
+      return {
+        ...state,
+        profile: null
       };
     default:
       return state;
